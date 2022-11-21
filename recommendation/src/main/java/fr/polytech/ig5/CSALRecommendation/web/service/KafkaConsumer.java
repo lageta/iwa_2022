51e2935f4c61feef307620eb05d6b6bedfb3e0c1
Axel Laget <axel.laget@gmail.com>
2022-11-21 22:37:43 +0100

    add mail template

--- recommendation/src/main/java/fr/polytech/ig5/CSALRecommendation/web/service/KafkaConsumer.java
+++ recommendation/src/main/java/fr/polytech/ig5/CSALRecommendation/web/service/KafkaConsumer.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import javax.mail.Multipart;
 import java.util.List;
 
 @Service
@@ -25,14 +26,15 @@ public class KafkaConsumer {
         for (User user : recommendationDao.recommendateUsers(offer.getOfferId())){
             String userMail = user.getMail();
             String recipient = userMail;
-            String body = "This new offer is matching your profile \n"+"\n" + offer.toString(); // TODO
+            String body = "This new offer is matching your profile \n" +"\n" + offer.toString(); // TODO
             String subject = "This new offer correspond to your profile !";
             MailDto mail = new MailDto();
             mail.setRecipient(recipient);
             mail.setBody(body);
             mail.setSubject(subject);
+            mail.setOffer(offer);
             System.out.println("Sending to: " + userMail);
-            String emailStatus = mailService.sendMail(mail);
+            String emailStatus = mailService.sendMailWithAttachment(mail);
         }
 
     }
