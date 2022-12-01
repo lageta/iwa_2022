4928051163d05044e2d9860ce00a26ae2d1be771
CemSARISOY <cem.sarisoy@etu.umontpellier.fr>
2022-12-01 13:41:47 +0100

    added offers to gateway

--- gateway/src/main/java/fr/polytech/ig5/CSALgateway/GatewayConfig.java
+++ gateway/src/main/java/fr/polytech/ig5/CSALgateway/GatewayConfig.java
@@ -15,12 +15,26 @@ public class GatewayConfig {
     @Bean
     public RouteLocator routes(RouteLocatorBuilder builder) {
         return builder.routes()
+                .route("offer-service", r -> r.path("/offer/**")
+                        .filters(f -> f.filter(filter))
+                        .uri("http://offers:8000"))
+                .route("offer-service", r -> r.path("/offers/**")
+                        .filters(f -> f.filter(filter))
+                        .uri("http://offers:8000"))
+                .route("offer-service", r -> r.path("/advantages/**")
+                        .filters(f -> f.filter(filter))
+                        .uri("http://offers:8000"))
+                .route("offer-service", r -> r.path("/keywords/**")
+                        .filters(f -> f.filter(filter))
+                        .uri("http://offers:8000"))
+
                 .route("user-service", r -> r.path("/user/**")
                         .filters(f -> f.filter(filter))
                         .uri("http://users:8081"))
                 .route("user-service", r -> r.path("/resume/**")
                         .filters(f -> f.filter(filter))
                         .uri("http://users:8081"))
+
                 .route("auth-service", r -> r.path("/auth/**")
                         .filters(f -> f.filter(filter))
                         .uri("http://auth:8082"))
